919fc5ca153dff2db579d9bfb018513e2b70a480
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-04-30 11:05:10 -0300

    Algunas validaciones para crear productos

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -13,9 +13,25 @@ exports.create = (req, res) => {
                 error: 'Image could not be uploaded'
             })
         }
+
+        // Check all fields
+        const { name, description, price, category, quantity, shipping } = fields
+
+        if(!name || !description || !price || !category || !quantity || !shipping){
+            return res.status(400).json({
+                error: 'All fields are required'
+            })
+        }
+
         let product = new Product(fields)
 
+        // check photo
         if (files.photo) {
+            if(files.photo.size > 1000000){
+                return res.status(400).json({
+                    error: 'Image should be less than 1mb size'
+                })
+            }
             product.photo.data = fs.readFileSync(files.photo.path)
             product.photo.contentType = files.photo.type
         }
