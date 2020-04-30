f67907ca4511e1d7e068ce1d0532489473db3073
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-04-30 12:40:16 -0300

    Delete product

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -22,6 +22,21 @@ exports.read = (req, res) => {
     return res.json(req.product)
 }
 
+exports.remove = (req, res) => {
+    let product = req.product
+    product.remove((err, deletedProduct) => {
+        if(err){
+            return res.status(400).json({
+                error: errorHandler(err)
+            })
+        }
+
+        res.json({
+            message: "Product deleted successfully"
+        })
+    })
+}
+
 
 exports.create = (req, res) => {
     let form = new formidable.IncomingForm()
--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read } = require('../controllers/product')
+const { create, productById, read, remove} = require('../controllers/product')
 const {requireSingin, isAuth, isAdmin } =  require('../controllers/auth')
 const { userById } = require('../controllers/user')
 
 router.get('/product/:productId', read)
-router.post('/product/create/:userId', requireSingin, isAuth, isAdmin, create);
+router.post('/product/create/:userId', requireSingin, isAuth, isAdmin, create)
+router.delete('/product/:productId/:userId', requireSingin, isAuth, isAdmin, remove)
 
 router.param('userId', userById)
 router.param('productId', productById)
