4e8660dcbd44e6e2011badb85ff9c8f758618c1e
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-04-30 09:54:19 -0300

    Ya puedo crear un producto

--- backend/src/controllers/category.js
+++ backend/src/controllers/category.js
@@ -1,12 +1,12 @@
 const Category = require('../models/category')
-const { errorHandle }= require('../helpers/dbErrorHandler')
+const { errorHandler }= require('../helpers/dbErrorHandler')
 
 exports.create = (req, res) => {
     const category = new Category(req.body)
     category.save((err, data) => {
         if(err){
             return res.status(400).json({
-                error: errorHandle(err)
+                error: errorHandler(err)
             })
         }
 
--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category')
+const { create } = require('../controllers/product')
 const {requireSingin, isAuth, isAdmin } =  require('../controllers/auth')
 const { userById } = require('../controllers/user')
 
-router.post('/category/create/:userId',
-    requireSingin,
-    isAuth,
-    isAdmin,
-    create);
+router.post('/product/create/:userId', requireSingin, isAuth, isAdmin, create);
 
 router.param('userId', userById)
 
