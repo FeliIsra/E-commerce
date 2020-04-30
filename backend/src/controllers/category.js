24b4f0d73414185fc58ad72425af5520ad342eac
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-04-30 13:29:32 -0300

    Category Update

--- backend/src/controllers/category.js
+++ backend/src/controllers/category.js
@@ -33,6 +33,19 @@ exports.remove = (req, res) => {
     })
 }
 
+exports.update = (req, res) => {
+    const category = req.category
+    category.name = req.body.name
+    category.save((err, data) => {
+        if(err){
+            return res.status(400).json({
+                error: 'Category is not exist'
+            })
+        }
+        res.json({ data })
+    } )
+}
+
 exports.categoryById = (req, res, next, id) => {
     Category.findById(id).exec((err, category) => {
         if(err || !category){
--- backend/src/routes/category.js
+++ backend/src/routes/category.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, categoryById, read, remove } = require('../controllers/category')
+const { create, categoryById, read, remove, update } = require('../controllers/category')
 const {requireSingin, isAuth, isAdmin } =  require('../controllers/auth')
 const { userById } = require('../controllers/user')
 
 router.get('/category/:categoryId', read)
 router.post('/category/create/:userId', requireSingin, isAuth, isAdmin, create)
 router.delete('/category/:categoryId/:userId', requireSingin, isAuth, isAdmin, remove)
+router.put('/category/:categoryId/:userId', requireSingin, isAuth, isAdmin, update)
 
 router.param('categoryId', categoryById)
 router.param('userId', userById)
