dcadcde5f2df4ecdee354792ff9e690de47b3838
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-05-02 18:22:09 -0300

    Read & Update Users

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,15 +1,18 @@
 const express = require('express');
 const router = express.Router();
 
-const { requireSingin, isAuth, isAdmin } = require('../controllers/auth')
+const { requireSingin, isAuth, isAdmin} = require('../controllers/auth')
 
-const { userById } = require('../controllers/user')
+const { userById, read, update } = require('../controllers/user')
 
 router.get('/secret/:userId', requireSingin, isAuth, isAdmin, (req, res) => {
     res.json({
         user: req.profile
     })
 });
+
+router.get('/user/:userId', requireSingin, isAuth, read)
+router.put('/user/:userId', requireSingin, isAuth, update)
 
 router.param('userId', userById)
 
