b5b7397c619192dd261d1366eca68be1e8019e42
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-05-02 18:02:18 -0300

    Get photo

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const { create, productById, read, remove, update, list, listRelated} = require('../controllers/product')
+const { create, productById, read, remove, update, list, listRelated, listCategories, listBySearch, photo} = require('../controllers/product')
 const {requireSingin, isAuth, isAdmin } =  require('../controllers/auth')
 const { userById } = require('../controllers/user')
 
@@ -9,9 +9,13 @@ router.get('/product/:productId', read)
 router.post('/product/create/:userId', requireSingin, isAuth, isAdmin, create)
 router.delete('/product/:productId/:userId', requireSingin, isAuth, isAdmin, remove)
 router.put('/product/:productId/:userId', requireSingin, isAuth, isAdmin, update)
+router.get('/product/photo/:productId', photo)
 
 router.get('/products', list)
 router.get('/products/related/:productId', listRelated)
+router.get('/products/categories', listCategories)
+router.post('/products/by/search', listBySearch)
+
 
 router.param('userId', userById)
 router.param('productId', productById)
