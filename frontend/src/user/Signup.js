a385ea904f871c96aa9bad66c2e799afaedb01ce
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-05-03 21:38:28 -0300

    Creo .env frontend

--- frontend/src/user/Signup.js
+++ frontend/src/user/Signup.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import Layout from "../core/Layout";
 
+
+
 function Signup(){
     return(
         <Layout title='Sign Up' description='Signup to Node React E-commerce App'>
