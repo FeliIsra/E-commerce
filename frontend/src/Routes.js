514aa985a0269b937d5d8b813e6af90357e756ba
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-05-05 20:07:32 -0300

    PrivateRoutes userDashboard

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Switch, Route} from 'react-router-dom'
 import Signup from "./user/Signup";
 import Signin from "./user/Signin";
 import Home from "./core/Home";
+import PrivateRoute from "./auth/PrivateRoutes";
+import UserDashboard from "./user/UserDashboard";
 
 function  Routes() {
     return (
@@ -11,6 +13,7 @@ function  Routes() {
                 <Route path="/" exact component={Home}/>
                 <Route path="/signin" exact component={Signin}/>
                 <Route path="/signup" exact component={Signup}/>
+                <PrivateRoute path='/dashboard' exact component={UserDashboard}/>
             </Switch>
         </BrowserRouter>
     )
