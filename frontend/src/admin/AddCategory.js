507f062d0c506cdc51b415a72762be54d12c560c
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-05-06 21:19:06 -0300

    Podemos crear categorias

--- frontend/src/admin/AddCategory.js
+++ frontend/src/admin/AddCategory.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import Layout from "../core/Layout";
 import { isAuthenticated } from "../auth";
 import { Link } from "react-router-dom";
+import { createCategory } from "./apiAdmin";
 
 function  AddCategory() {
     const [name, setName] = useState('')
@@ -23,6 +24,38 @@ function  AddCategory() {
         setSuccess(false)
 
         // make request to API to create category
+
+        createCategory(user._id, token, {name})
+            .then(data => {
+                if(data.error){
+                    setError(true)
+                } else {
+                    setError('')
+                    setSuccess(true)
+                }
+            })
+    }
+
+    const showSucces = () => {
+        if(success){
+            return <h3 className={'text-success'}>{name} is created</h3>
+        }
+    }
+
+    const showError = () => {
+        if(error){
+            return <h3 className={'text-danger'}>Cateogory should be unique</h3>
+        }
+    }
+
+    const goBack = () => {
+        return(
+            <div className={'mt-5'}>
+                <Link to={'/admin/dashboard'} className={'text-warning'}>
+                    Back to Dashboard
+                </Link>
+            </div>
+        )
     }
 
     const newCategoryForm = () => {
@@ -30,7 +63,13 @@ function  AddCategory() {
             <form onSubmit={clickSubmit}>
                 <div className={'form-group'}>
                     <label className={'text-muted'}>Name</label>
-                    <input type={'text'} className={'form-control'} onChange={handleChange} value={name} autoFocus/>
+                    <input type={'text'}
+                           className={'form-control'}
+                           onChange={handleChange}
+                           value={name}
+                           autoFocus
+                           required
+                    />
                 </div>
                 <button className={'btn btn-outline-primary'}>Create category</button>
             </form>
@@ -38,10 +77,13 @@ function  AddCategory() {
     }
 
     return(
-        <Layout title={'Add a new Category'} description={`G'dat ${name}, ready to create a new category?`}  >
+        <Layout title={'Add a new Category'} description={`G'day ${user.name}, ready to create a new category?`}  >
             <div className={'row'}>
                 <div className={'col-md-8 offset-md-2'}>
+                    {showError()}
+                    {showSucces()}
                     {newCategoryForm()}
+                    {goBack()}
                 </div>
             </div>
         </Layout>
