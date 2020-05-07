f0c200124ed6af25bfaeaff4cf36fba0ac01f416
FeliIsra <fisrael@est.frba.utn.edu.ar>
2020-05-07 00:27:20 -0300

    Creo el form del AddProduct.js

--- frontend/src/admin/AddProduct.js
+++ frontend/src/admin/AddProduct.js
@@ -5,14 +5,115 @@ import { Link } from "react-router-dom";
 import { createProduct } from "./apiAdmin";
 
 
-const { user, token } = isAuthenticated()
+
 
 function  AddProduct() {
+
+    const { user, token } = isAuthenticated()
+    const [ values, setValues ] = useState({
+        name: '',
+        description: '',
+        price: '',
+        categories: [],
+        category: '',
+        shipping: '',
+        quantity: '',
+        photo: '',
+        loading: false,
+        error: '',
+        createdProduct: '',
+        redirectToProfile: false,
+        formData: ''
+    })
+
+    const {
+        name,
+        description,
+        price,
+        categories,
+        category,
+        shipping,
+        quantity,
+        loading,
+        error,
+        createdProduct,
+        redirectToProfile,
+        formData
+    } = values
+
+    useEffect(() => {
+        setValues({...values, formData: new FormData()})
+    }, [])
+
+    const handleChange = name => event => {
+        const value = name === 'photo' ? event.target.files[0] : event.target.value
+        formData.set(name, value)
+        setValues({
+            ...values,
+            [name]: value
+        })
+    }
+
+    const clickSubmit = (e) => {
+        //
+    }
+
+    const newProductForm = () => {
+        return(
+            <form className={'mb-3'} onClick={clickSubmit}>
+
+                <h4>Post photo</h4>
+                <div className={'form-group'}>
+                    <label className={'btn btn-secondary'}>
+                        <input onChange={handleChange('photo')} type={'file'} name={'photo'} accept={'image/*'}/>
+                    </label>
+                </div>
+
+                <div className="form-group">
+                    <label className={'text-muted'}>Name</label>
+                    <input onChange={handleChange('name')} type="text" className={'form-control'} value={name}/>
+                </div>
+
+                <div className="form-group">
+                    <label className={'text-muted'}>Description</label>
+                    <textarea onChange={handleChange('description')} className={'form-control'} value={description}/>
+                </div>
+
+                <div className="form-group">
+                    <label className={'text-muted'}>Price</label>
+                    <input onChange={handleChange('price')} type="number" className={'form-control'} value={price}/>
+                </div>
+
+                <div className="form-group">
+                    <label className={'text-muted'}>Category</label>
+                    <select onChange={handleChange('category')} className={'form-control'}>
+                        <option value="5eaab6d1cee9c04644e71557">Node</option>
+                    </select>
+                </div>
+
+                <div className="form-group">
+                    <label className={'text-muted'}>Shipping</label>
+                    <select onChange={handleChange('shipping')} className={'form-control'}>
+                        <option value="0">No</option>
+                        <option value="1">Yes</option>
+                    </select>
+                </div>
+
+                <div className="form-group">
+                    <label className={'text-muted'}>Quantity</label>
+                    <input onChange={handleChange('quantity')} type="number" className={'form-control'} value={quantity}/>
+                </div>
+
+                <button className="btn btn-outline-primary">Create product</button>
+            </form>
+        )
+    }
+
     return (
         <Layout title={'Add a new Product'} description={`G'day ${user.name}, ready to create a new product?`}  >
             <div className={'row'}>
                 <div className={'col-md-8 offset-md-2'}>
-                    ...
+                    {newProductForm()}
                 </div>
             </div>
         </Layout>
